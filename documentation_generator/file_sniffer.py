77ae51141365e2d92425b0fc13f143ad76894c0d
John Edmiston <john.edmiston@hingehealth.com>
2019-02-16 23:06:06 -0800

    add docstring example

--- documentation_generator/file_sniffer.py
+++ documentation_generator/file_sniffer.py
@@ -1,17 +1,41 @@
-def detect_multi_line_assignment(kk, lines):
-    l = lines[kk]
-    if l.count('=') == 0:
-        return {'kk':kk + 1, 'continue':1, 'output':None}
+"""
 
-    kk0 = kk
+"""
+
+def detect_multi_line_assignment(index, lines):
+    """
+    get multi line assignment e.g. if this string is 
+    split on multiple lines
+    xs = np.array([[1, 2, 3],
+                   [5, 6, 8]])
+    becomes in the line listing
+    ["xs = np.array([[1, 2, 3],\n",[5, 6, 8]])]
+    this function will return the entire continuous output e.g. 
+    "xs = np.array([[1, 2, 3],
+                   [5, 6, 8]])"
+
+    @index: integer index of a list (line number)
+    @lines: list of strings (lines from a file)
+    """
+    l = lines[index]
+    if l.count('=') == 0:
+        return {'index':index + 1, 'continue':1, 'output':None}
+
+    initial_index = index
     startline = ""
-    l = lines[kk0]
-    while l.strip()[-1] == ',' and kk < len(lines):
-        l = lines[kk]
+    l = lines[initial_index]
+    # lines ending in comma are continuations
+    # pd.DataFrame.from_dict({'x':[1, 2],
+    #                         'y':[2, 3]})
+    while l.strip()[-1] == ',' and index < len(lines):
+        l = lines[index]
         startline += l
-        kk += 1
+        index += 1
         pass
-    if kk == kk0:
-        startline = lines[kk0]
-    return {'kk':kk + 1, 'continue':0, 'output':startline}
+    
+    if index == initial_index:
+        startline = lines[initial_index]
+        index = index + 1
+        
+    return {'index':index, 'continue':0, 'output':startline}
     
