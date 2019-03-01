db76e0dce8c4314fc05e941789510b7e6018e7db
John Edmiston <john.edmiston@hingehealth.com>
2019-03-01 13:30:36 -0800

    Markdown add fixes to markdown capability from raw py files

--- documentation_generator/docgen.py
+++ documentation_generator/docgen.py
@@ -5,7 +5,7 @@ useful for generating doc strings which update from a working python script.
 import argparse
 import pandas as pd
 from documentation_generator.file_sniffer import detect_multi_line_assignment
-from documentation_generator.file_sniffer import detect_multi_line_function
+from documentation_generator.file_sniffer import detect_multi_line_function, detect_multi_line_markdown_block
 from documentation_generator.generate_ipynb import generate_ipynb
 from documentation_generator.file_sniffer import get_text_from_file, write_text_to_file
 
@@ -42,7 +42,8 @@ if __name__ == "__main__":
 
         outval = detect_multi_line_assignment(file_line_index, lines)
         outval2 = detect_multi_line_function(file_line_index, lines)
-
+        outval3 = detect_multi_line_markdown_block(file_line_index, lines)
+        
         # TODO: wrap this in function
         # a function was detected
         if outval2["continue"] == 0:
@@ -50,6 +51,13 @@ if __name__ == "__main__":
             file_line_index = outval2["index"]
             printfunc(output)
             jupyters += "#code\n%s\n#endcode\n" % (output)
+            continue
+
+        if outval3["continue"] == 0:
+            output = outval3["output"]
+            file_line_index = outval3["index"]
+            printfunc(output)
+            jupyters += "%s\n" % output
             continue
         
         file_line_index = outval["index"] # increment forward
@@ -87,6 +95,8 @@ if __name__ == "__main__":
         # set up a pre-jupyter document by encolsing blocks in #code/#endcode blocks
         source_jupyter_file = args.source.replace('.py', '_tmp.py')
         tmp = open(source_jupyter_file, 'w')
+
+
         tmp.write(jupyters.replace('\n\n','\n'))
         # code needs to end in # (TODO)
         tmp.write("\n#\n")
